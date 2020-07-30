c06c89bed255a9dd196e56fe64227f462142f20d
Guilherme Costa <guilherme.costa@itbam.org.br>
2020-07-30 18:00:59 -0400

    Initial Job Controller

---
+++ src/main/java/com/dvlcube/app/controller/JobController.java
@@ -0,0 +1,63 @@
+package com.dvlcube.app.controller;
+
+import com.dvlcube.app.dto.JobDTO;
+import com.dvlcube.app.dto.filter.JobFilterDTO;
+import com.dvlcube.app.interfaces.MenuItem;
+import com.dvlcube.app.manager.data.e.Menu;
+import com.dvlcube.app.mapper.JobMapper;
+import com.dvlcube.app.model.Job;
+import com.dvlcube.app.service.JobService;
+import com.dvlcube.utils.GenericMapper;
+import com.dvlcube.utils.GenericService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@MenuItem(value = Menu.JOB)
+@RequestMapping("${dvl.rest.prefix}/job")
+public class JobController implements ListRest<Job, JobMapper, JobFilterDTO, JobDTO, Long> {
+
+    private JobService service;
+
+    @Override
+    public GenericMapper<Job, JobDTO> getMapper() {
+        return new JobMapper();
+    }
+
+    @Override
+    public JobMapper getFilterMapper() {
+        return new JobMapper();
+    }
+
+    @Override
+    public GenericService<Job, Long> getService() {
+        return service;
+    }
+
+    @Autowired
+    public JobController(JobService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ApiOperation("Create Job")
+    public ResponseEntity<JobDTO> add(@ApiParam(value = "JobDTO", required = true)
+                                          @Valid @RequestBody JobDTO dto){
+        return service.add(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobDTO> find(@ApiParam(value = "id", required = true) Long id){
+        return service.find(id);
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(@ApiParam(value = "id", required = true) Long id){
+        return service.delete(id);
+    }
+}
--- src/main/java/com/dvlcube/app/service/impl/JobServiceImpl.java
+++ src/main/java/com/dvlcube/app/service/impl/JobServiceImpl.java
@@ -1,11 +1,13 @@
 package com.dvlcube.app.service.impl;
 
+import com.dvlcube.app.dto.JobDTO;
 import com.dvlcube.app.mapper.JobMapper;
 import com.dvlcube.app.model.Job;
 import com.dvlcube.app.repository.JobRepository;
 import com.dvlcube.app.service.JobService;
 import com.dvlcube.utils.GenericServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +20,21 @@ public class JobServiceImpl extends GenericServiceImpl<Job, Long> implements Job
 
     public JobServiceImpl() {
         this.mapper = new JobMapper();
+    }
+
+    @Override
+    public ResponseEntity add(JobDTO dto) {
+        mapper.convertToDto(super.add(mapper.convertToEntity(dto)));
+        return null;
+    }
+
+    @Override
+    public ResponseEntity find(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity delete(Long id) {
+        return null;
     }
 }
