64bb07731b6e7c1f422d522e6940a8c9f32f4617
Reinado Silva <rein.ant@gmail.com>
2021-04-21 17:48:02 -0300

    Alterações

---
+++ src/main/java/com/dvlcube/app/jpa/repo/JobRepository.java
@@ -0,0 +1,10 @@
+package com.dvlcube.app.jpa.repo;
+
+import com.dvlcube.app.jpa.BasicRepository;
+import com.dvlcube.app.jpa.DvlRepository;
+import com.dvlcube.app.manager.data.JobBean;
+
+
+public interface JobRepository extends DvlRepository<JobBean, Long>, BasicRepository<JobBean, Long> {
+
+}
---
+++ src/main/java/com/dvlcube/app/rest/JobService.java
@@ -0,0 +1,83 @@
+package com.dvlcube.app.rest;
+
+import static com.dvlcube.app.manager.data.e.Menu.CONFIGURATION;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dvlcube.app.interfaces.MenuItem;
+import com.dvlcube.app.jpa.repo.JobRepository;
+import com.dvlcube.app.manager.data.JobBean;
+import com.dvlcube.app.manager.data.vo.MxRestResponse;
+import com.dvlcube.utils.interfaces.rest.MxFilterableBeanService;
+
+@RestController
+@MenuItem(value = CONFIGURATION)
+@RequestMapping("${dvl.rest.prefix}/jobs")
+public class JobService implements MxFilterableBeanService<JobBean, Long>{
+
+	@Autowired
+	private JobRepository repo;
+	
+	@Override
+	@GetMapping
+	public Iterable<JobBean> get(Map<String, String> params) {
+		// TODO Auto-generated method stub
+		return repo.firstPage();
+	}
+
+	@Override
+	@GetMapping("/{id}")
+	public Optional<JobBean> get(@PathVariable Long id ) {
+		// TODO Auto-generated method stub
+		return repo.findById(id);
+	}
+
+	@Override
+	@PostMapping
+	public MxRestResponse post(@Valid @RequestBody JobBean body) {
+		// TODO Auto-generated method stub
+		JobBean save = repo.save(body);
+		return GenericRestResponse.ok(save.getId());
+		
+	}
+	
+	
+	@GetMapping("/filtered")
+	public List<JobBean> getFiltered(@RequestParam Map<String, String> params) {
+		return repo.findAllBy(params);
+			
+		
+	}
+	
+	@GetMapping("/group/{group}/filtered")
+	public List<JobBean> getGroupFiltered(@PathVariable String group, @RequestParam Map<String, String> params) {
+		return repo.findAllBy(params, group);
+	}
+	
+	@GetMapping("/like")
+	public Iterable<JobBean> getLike(@RequestParam(required = true) String id) {
+		return repo.findAllLike(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repo.deleteById(id);
+	}
+	
+
+
+}
